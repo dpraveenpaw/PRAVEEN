dd7dcf192747483f97daf12e0c3ec62253a88292
dpraveenpaw <d.praveen4you@gmail.com>
2024-11-25 17:22:06 +0530

    new feature by f1

--- my.java
+++ my.java
@@ -26,6 +26,25 @@ class Main {
       if (result == number) {
         System.out.print(number + " ");
       }
+    } 
+ }
+ #adding new feature by f1
+ import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class AppendFile {
+
+    public static void main(String[] args) {
+
+        String path = System.getProperty("user.dir") + "\\src\\test.txt";
+        String text = "Added text";
+
+        try {
+            Files.write(Paths.get(path), text.getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+        }
     }
-  }
+}
 }
