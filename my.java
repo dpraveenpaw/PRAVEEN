db73d4f66669cfbef164772e1e4c02331c353710
dpraveenpaw <d.praveen4you@gmail.com>
2024-11-26 15:52:29 +0530

    Update my.java

--- my.java
+++ my.java
@@ -38,13 +38,3 @@ public class AppendFile {
 
     public static void main(String[] args) {
 
-        String path = System.getProperty("user.dir") + "\\src\\test.txt";
-        String text = "Added text";
-
-        try {
-            Files.write(Paths.get(path), text.getBytes(), StandardOpenOption.APPEND);
-        } catch (IOException e) {
-        }
-    }
-
-  }
